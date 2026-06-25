9e5c9a1f1e3b220c33f69a367e4f5109366acc45
mudasir najimudin <mudassirnajimuddin@gmail.com>
2026-06-25 17:06:39 +0300

    refactor sign-in page to remove Suspense wrapper and update tsconfig to include additional type definitions

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,5 @@
-import { Suspense } from "react";
 import { SignInForm } from "./sign-in-form";
 
 export default function SignInPage() {
-  return (
-    <Suspense>
-      <SignInForm />
-    </Suspense>
-  );
+  return <SignInForm />;
 }
